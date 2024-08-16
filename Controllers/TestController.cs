e9f257817c1b71d3594022ff9cae38c1fee84582
Lenovo <Lenovo@DESKTOP-KIPHLHP>
2024-08-16 17:23:19 +0530

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DummyMVCwithoutEF.Models;
+
+namespace DummyMVCwithoutEF.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        ProductDal pd=new ProductDal();
+        public ActionResult Index()
+        {
+            var data=pd.GetAllProducts();
+            return View(data);
+        }
+
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct(Product p)
+        {
+           pd.AddProduct(p);
+           TempData["success"] = "Product Added Successfully";
+           return RedirectToAction("AddProduct");
+        }
+
+        public ActionResult DeleteProd(int id)
+        {
+            pd.DeleteProd(id);
+            TempData["Deleted"] = "Product Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+
+
+      
+
+        public ActionResult EditProd(int id, string name, string cat, double price)
+        {
+            pd.EditProd(id,name,cat,price);
+            TempData["Updated"] = "Product Updated Successfully";
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Models/ProductDal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DummyMVCwithoutEF.Models
+{
+    public class ProductDal
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+        SqlConnection conn;
+
+        public ProductDal() 
+        {
+            conn = new SqlConnection(cs);
+            conn.Open();    
+        }  //Adding Connection 
+        public List<Product> GetAllProducts()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("exec sp_show", conn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            List<Product> product = new List<Product>();   
+            foreach (DataRow dr in dt.Rows) 
+            {
+                product.Add
+                    (
+                    new Product
+                    {
+                        Id = int.Parse(dr["id"].ToString()),
+                        pname = dr["pname"].ToString(),
+                        pcat = dr["pcat"].ToString(),
+                        price = double.Parse(dr["price"].ToString())
+                    }
+                    );
+            }
+            return product;
+
+        }  //fetching data in list format
+
+
+        public void AddProduct(Product p)
+        {
+            string q = $"exec sp_insert'{p.pname}','{p.pcat}',{p.price}";
+            SqlCommand cmd = new SqlCommand(q, conn);
+            cmd.ExecuteNonQuery();
+        } // Inserting data 
+        public void DeleteProd(int id)
+        {
+            
+            string q = $"exec sp_delete {id}";
+            SqlCommand cmd = new SqlCommand(q, conn);
+            cmd.ExecuteNonQuery();
+        } // Deleting Data
+
+
+
+        public void EditProd(int id,string name,string cat,double price)
+        {
+            string q = $"exec sp_edit {id},'{name}','{cat}','{price}'";
+            SqlCommand cmd = new SqlCommand(q, conn);
+            cmd.ExecuteNonQuery();
+        } // Updating Data
+    }
+}
